b96a6819c8da9876500e88fc500621dade96eca6
abdel <aboualyet63@gmail.com>
2020-09-25 15:46:20 -0400

    added my personal image to app.js

--- Desktop/JavaScript/CMP464/myapp/src/App.js
+++ Desktop/JavaScript/CMP464/myapp/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './aboulayeTchakoura.jpeg';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         My name is Aboulaye Tchakoura and I'm goint to master React!
+         My name is Aboulaye Tchakoura and I'm going to master React!
         </p>
         <a
           className="App-link"
