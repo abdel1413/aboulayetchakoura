7985c091a2480b9ec194152225f10e466a629e37
abdel <aboualyet63@gmail.com>
2020-09-26 18:40:36 -0400

    HW1 is submitted

--- Desktop/JavaScript/CMP464/myapp/src/App.js
+++ Desktop/JavaScript/CMP464/myapp/src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/abdel1413/myapp.git"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Check Me Out!
         </a>
       </header>
     </div>
