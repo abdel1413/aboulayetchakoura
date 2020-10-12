ac71ed3cccf9c633df7ed73bb1b632ef055cec64
abdel <aboualyet63@gmail.com>
2020-10-12 00:18:35 -0400

    modified App.js to say Aboulaye Tchakoura

--- Desktop/aboulayetchakoura.github.io/src/App.js
+++ Desktop/aboulayetchakoura.github.io/src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           My name is Aboulaye Tchakoura
+          and I am going to master react
         </p>
         <a
           className="App-link"
